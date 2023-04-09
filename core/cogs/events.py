044d12d695a5a6d10855457452cbb1f515ca1bdc
feluxerich <felix.waidler@web.de>
2023-04-09 23:52:44 +0200

    fixed channel removal in temp voice feature

--- core/cogs/events.py
+++ core/cogs/events.py
@@ -1,4 +1,4 @@
-from discord import Member, VoiceChannel, VoiceState, StageChannel
+from discord import Member, VoiceState
 from discord.ext.commands import Cog
 
 from utils.bot import ExtendedBot
@@ -8,7 +8,7 @@ from utils.guild import update_member_count
 class EventsCog(Cog):
     def __init__(self, client: ExtendedBot):
         self.client = client
-        self.talks: list[VoiceChannel] = []
+        self.talks: list[int] = []
 
     @Cog.listener()
     async def on_member_join(self, _):
@@ -20,17 +20,17 @@ class EventsCog(Cog):
 
     @Cog.listener()
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
-        if type(before.channel) == StageChannel or type(after.channel) == StageChannel \
-                or not after.channel or after.channel.id != self.client.config.CREATE_VOICE_CHANNEL:
+        if after.channel and after.channel.id != self.client.config.CREATE_VOICE_CHANNEL:
             return
-        if before.channel in self.talks and len(before.channel.members) == 0:
+        if before.channel and before.channel.id in self.talks and len(before.channel.members) == 0:
+            self.talks.remove(before.channel.id)
             await before.channel.delete(reason='talk_empty')
-            self.talks.remove(before.channel)
+            return
         new_voice = await after.channel.category.create_voice_channel(
             f'{member.display_name}\'s Talk',
             reason='talk_created'
         )
-        self.talks.append(new_voice)
+        self.talks.append(new_voice.id)
         await member.move_to(channel=new_voice, reason='member_moved')
 
 
