e4f3024dc697fc009aff93e439e328a648dbc5b3
feluxerich <felix.waidler@web.de>
2023-04-11 00:06:04 +0200

    added command error handler

--- core/main.py
+++ core/main.py
@@ -1,12 +1,15 @@
 from asyncio import sleep
 from os import getenv
 from pathlib import Path
+from typing import Any
 
-from discord import Intents, Game, Status
+from discord import Intents, Game, Status, Interaction
+from discord.app_commands import MissingPermissions
 
 from utils import ExtendedBot
 from utils.config import load_config
 from utils.guild import update_member_count
+from utils.message import send, make_embed
 
 client = ExtendedBot(
     config=load_config(path=Path('config.yaml')),
@@ -23,11 +26,24 @@ async def on_ready():
     client.loop.create_task(status_task())
 
 
+@client.tree.error
+async def on_app_command_error(interaction: Interaction, error: Any):
+    error_messages = {
+        MissingPermissions: 'Missing Permissions'
+    }
+    await send(interaction=interaction, embed=make_embed(
+        client,
+        'error',
+        error_message=error_messages[type(error)]
+    ))
+
+
 async def status_task():
     while True:
         for status in client.config.PRESENCES:
             await client.change_presence(activity=Game(status), status=Status.online)
             await sleep(client.config.PRESENCE_TIMEOUT)
+
 
 if __name__ == '__main__':
     client.run(getenv('BOT_TOKEN'))
